9ef85328b8f517436e9d36fae561d7014eb82758
jtirana98 <joana.tirana98@gmail.com>
2024-06-25 14:55:40 +0200

    integrating label-skew into experiment code

--- experiment.py
+++ experiment.py
@@ -31,7 +31,7 @@ parser.add_argument('--device', type=str, default='cpu', help='The device to run
 parser.add_argument('--expname', type=str, default='MergeSFL')
 parser.add_argument('--two_splits', action="store_true", help='do U-Shape')
 parser.add_argument('--type_noniid', type=str, default='default')
-parser.add_argument('--level', type=int, default=10)
+parser.add_argument('--level', type=str, default='noniid-#label1')
 
 
 args = parser.parse_args()
@@ -132,8 +132,12 @@ def main():
     print(args.__dict__)
 
     if args.two_splits:
-        client_global_model, server_global_model = models.create_model_instance_SL_two_splits(args.dataset_type, args.model_type, 1)
-        nets, _ = models.create_model_instance_SL_two_splits(args.dataset_type, args.model_type, worker_num)
+        m = 4
+        if args.type_noniid == 'label_skew':
+            m = 8
+        
+        client_global_model, server_global_model = models.create_model_instance_SL_two_splits(args.dataset_type, args.model_type, 1, m=m)
+        nets, _ = models.create_model_instance_SL_two_splits(args.dataset_type, args.model_type, worker_num, m=m)
 
         client_global_model_first = client_global_model[0][0]
         client_global_model_last = client_global_model[0][1]
@@ -146,8 +150,12 @@ def main():
             net[0].load_state_dict(global_model_par_first)
             net[1].load_state_dict(global_model_par_last)
     else:
-        client_global_model, server_global_model = models.create_model_instance_SL(args.dataset_type, args.model_type, 1)
-        nets, _ = models.create_model_instance_SL(args.dataset_type, args.model_type, worker_num)
+        m = 4
+        if args.type_noniid == 'label_skew':
+            m = 8
+
+        client_global_model, server_global_model = models.create_model_instance_SL(args.dataset_type, args.model_type, 1, m=m)
+        nets, _ = models.create_model_instance_SL(args.dataset_type, args.model_type, worker_num, m=m)
 
         client_global_model = client_global_model[0]
         global_model_par = client_global_model.state_dict()
@@ -157,20 +165,32 @@ def main():
     # Create model instance
     if args.type_noniid == 'default':
         train_dataset, test_dataset, train_data_partition, labels = partition_data(args.dataset_type, args.data_pattern, worker_num)
+    elif args.type_noniid == 'label_skew':
+        X_train, y_train, X_test, y_test, net_dataidx_map, traindata_cls_counts = datasets.partition_data_label_skew(
+        args.dataset_type, './data', args.level, worker_num)
+        labels = False
     else:
         train_dataset, test_dataset, train_data_partition, labels = partition_data_non_iid_strict(args.dataset_type, args.data_pattern, worker_num)
-
-    if labels:
-        test_loader = datasets.create_dataloaders(test_dataset, batch_size=64, shuffle=False, collate_fn=lambda x: datasets.collate_fn(x, labels))
+        
+    if args.type_noniid != 'label_skew':
+        if labels:
+            test_loader = datasets.create_dataloaders(test_dataset, batch_size=64, shuffle=False, collate_fn=lambda x: datasets.collate_fn(x, labels))
+        else:
+            test_loader = datasets.create_dataloaders(test_dataset, batch_size=64, shuffle=False)
     else:
-        test_loader = datasets.create_dataloaders(test_dataset, batch_size=64, shuffle=False)
+        train_dl_local_, test_dl_local, _, _ = datasets.get_dataloader_skew(args.dataset_type, './data', args.batch_size, 32, model=args.model_type)
+        test_loader = test_dl_local
 
     # Clients data loaders
     bsz_list = np.ones(worker_num, dtype=int) * args.batch_size
     client_train_loader = []
     for worker_idx in range(worker_num):
-        if labels:
+        if args.type_noniid != 'label_skew' and labels:
             client_train_loader.append(datasets.create_dataloaders(train_dataset, batch_size=int(bsz_list[worker_idx]), selected_idxs=train_data_partition.use(worker_idx), pin_memory=False, drop_last=True, collate_fn=lambda x: datasets.collate_fn(x, labels)))
+        elif args.type_noniid == 'label_skew':
+            dataidxs_ = net_dataidx_map[worker_idx]
+            train_dl_local_, test_dl_local, _, _ = datasets.get_dataloader_skew(args.dataset_type, './data', args.batch_size, 32, dataidxs_, 0, model=args.model_type)
+            client_train_loader.append(train_dl_local_)
         else:
             print(f'for worker {worker_idx}')
             client_train_loader.append(datasets.create_dataloaders(train_dataset, batch_size=int(bsz_list[worker_idx]), selected_idxs=train_data_partition.use(worker_idx), pin_memory=False, drop_last=True))
@@ -178,7 +198,9 @@ def main():
     epoch_client_lr = args.client_lr
     epoch_server_lr = args.server_lr
     print('Start training')
+    index_data = 0
     for epoch_idx in range(1, 1 + args.epoch):
+        
         print(f'In epoch:{epoch_idx}')
         start_time = time.time()
         # Learning rate adjustment
@@ -224,7 +246,10 @@ def main():
                 my_outas = []
                 clients_part1_send_targets = []
                 for worker_idx in range(worker_num):
-                    inputs, targets = next(client_train_loader[worker_idx])
+                    if args.type_noniid != 'label_skew':
+                        inputs, targets = next(client_train_loader[worker_idx])
+                    else:
+                        inputs, targets = client_train_loader[worker_idx][index_data]
 
                     inputs, targets = inputs.to(device), targets.to(device) 
                     clients_part1_send_targets.append(targets)
@@ -281,7 +306,7 @@ def main():
                     # server sends grad_a to individual workers
                     my_outas[worker_idx].backward(grad_a)
                     client_optimizers_first[worker_idx].step()
-
+                index_data = (index_data+1) %  len(client_train_loader[0])
             else:
                 clients_smash_data = []
                 clients_send_data = []
@@ -289,7 +314,10 @@ def main():
 
                 sum_bsz = sum([bsz_list[i] for i in range(worker_num)])
                 for worker_idx in range(worker_num):
-                    inputs, targets = next(client_train_loader[worker_idx])
+                    if args.type_noniid != 'label_skew':
+                        inputs, targets = next(client_train_loader[worker_idx])
+                    else:
+                        inputs, targets = next(iter(client_train_loader[worker_idx]))
 
                     inputs, targets = inputs.to(device), targets.to(device)
 
@@ -302,14 +330,14 @@ def main():
                     clients_send_targets.append(targets)
                 
                 m_data = torch.cat(clients_send_data, dim=0)
-                print(f'combined data shape:{m_data.shape}')
+                #print(f'combined data shape:{m_data.shape}')
                 m_target = torch.cat(clients_send_targets, dim=0)
 
                 m_data.requires_grad_()
 
                 # server side fp
                 outputs = server_global_model(m_data)
-                print(f'output shape:{outputs.shape}')
+                #print(f'output shape:{outputs.shape}')
 
                 loss = F.cross_entropy(outputs, m_target.long())
 
@@ -328,7 +356,7 @@ def main():
                     clients_optimizers[worker_idx].zero_grad()
                     clients_smash_data[worker_idx].backward(clients_grad.to(device))
                     clients_optimizers[worker_idx].step()
-
+                index_data = (index_data+1) %  len(client_train_loader[0])
         # AGGREGATION
         with torch.no_grad():
             for worker_idx in range(worker_num):
@@ -367,10 +395,14 @@ def main():
                     client_global_model.load_state_dict(global_model_par)
         
         server_global_model.to('cpu')
-        if args.two_splits:
-            test_loss, acc = test((client_global_model_first, client_global_model_last), server_global_model, test_loader, two_split=args.two_splits)
+        if args.type_noniid != 'label_skew':
+            test_loader_ = test_loader.loader
         else:
-            test_loss, acc = test(client_global_model, server_global_model, test_loader, two_split=args.two_splits)
+            test_loader_ = test_loader
+        if args.two_splits:
+            test_loss, acc = test((client_global_model_first, client_global_model_last), server_global_model, test_loader_, two_split=args.two_splits)
+        else:
+            test_loss, acc = test(client_global_model, server_global_model, test_loader_, two_split=args.two_splits)
         print("Epoch: {}, accuracy: {}, test_loss: {}".format(epoch_idx, acc, test_loss))
 
         if args.two_splits:

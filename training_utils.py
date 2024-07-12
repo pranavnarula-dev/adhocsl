490db56aefd6ebb64d61beafff07df0f4be7620a
Pranav Narula <67228332+pranavnarula-dev@users.noreply.github.com>
2024-07-12 15:48:31 +0100

    Update training_utils.py
    allowing handling of 2 intermediate servers in loss calculation

--- training_utils.py
+++ training_utils.py
@@ -3,22 +3,25 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-
 def test(model_fe, model_p, data_loader, device=torch.device("cpu"), two_split=False):
-    model_p.eval()
+    if isinstance(model_p, list):
+        for model in model_p:
+            model.eval()
+    else:
+        model_p.eval()
+
     if two_split:
         model_fe[0].eval()
         model_fe[1].eval()
     else: 
         model_fe.eval()
 
-    #data_loader = data_loader.loader
+    data_loader = data_loader.loader
     test_loss = 0.0
     test_accuracy = 0.0
     correct = 0
     with torch.no_grad():
         for data, target in data_loader:
-
             data, target = data.to(device), target.to(device)
 
             if two_split:
@@ -26,7 +29,11 @@ def test(model_fe, model_p, data_loader, device=torch.device("cpu"), two_split=F
             else:
                 output1 = model_fe(data)
             
-            output = model_p(output1)
+            if isinstance(model_p, list):
+                # In case of multiple server models, use the first one for testing
+                output = model_p[0](output1)
+            else:
+                output = model_p(output1)
             
             if two_split:
                 output = model_fe[1](output)
@@ -43,4 +50,3 @@ def test(model_fe, model_p, data_loader, device=torch.device("cpu"), two_split=F
     test_loss /= len(data_loader.dataset)
     test_accuracy = np.float64(1.0 * correct / len(data_loader.dataset))
     return test_loss, test_accuracy
-
